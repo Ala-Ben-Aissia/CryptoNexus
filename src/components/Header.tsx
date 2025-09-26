8df1302b0aa2d8b3d0de9bc5b16eff554afede9e
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-09-26 08:25:57 +0100

    project setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div style={{ display: 'flex', gap: '12px' }}>
+      <img
+        src="crypto.webp"
+        alt="crypto logo"
+        style={{ alignSelf: 'center' }}
+      />
+      <h1 style={{ marginBottom: 0 }}>Crypto Nexus</h1>
+    </div>
+  )
+}
