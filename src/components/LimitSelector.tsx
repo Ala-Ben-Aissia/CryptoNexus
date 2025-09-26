c4d78b6eb233d35ab6d0c074d1bb3d952b47451c
Ala Ben Aissia <xalabenaissiax@outlook.com>
2025-09-26 17:02:15 +0100

    separate select from label

--- src/components/LimitSelector.tsx
+++ src/components/LimitSelector.tsx
@@ -9,23 +9,21 @@ export default function LimitSelector({
 }) {
   return (
     <div className="controls">
-      <label htmlFor="limit">
-        Show:
-        <select
-          name="limit"
-          id="limit"
-          value={limit}
-          onChange={(e) => {
-            onLimitChange(+e.currentTarget.value)
-          }}
-        >
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="20">20</option>
-          <option value="50">50</option>
-          <option value="100">100</option>
-        </select>
-      </label>
+      <label htmlFor="limit">Show:</label>
+      <select
+        name="limit"
+        id="limit"
+        value={limit}
+        onChange={(e) => {
+          onLimitChange(+e.currentTarget.value)
+        }}
+      >
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
     </div>
   )
 }
